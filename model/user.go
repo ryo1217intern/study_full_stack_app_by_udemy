225fd9ec2dd80e7d561a7449ca285fc58e47ae1f
ryo1217intern <135844555+ryo1217intern@users.noreply.github.com>
2023-11-21 09:40:12 +0900

    CreatedAtとUpdatedAtのjsonの値がダブルクォーとで囲まれていなかったので修正。

--- model/user.go
+++ model/user.go
@@ -6,8 +6,8 @@ type User struct {
 	ID uint `json:"id" gorm:"primaryKey"`
 	Email string `json:"email" gorm:"unique"`
 	Password string `json:"password"`
-	CreatedAt time.Time `json:"created_at`
-	UpdatedAt time.Time	`json:"updated_at`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time	`json:"updated_at"`
 }
 
 type UserResponse struct {
