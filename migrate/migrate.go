3a2ed5133aecbe6e0dfdca090da50931b1f919d3
ryo1217intern <135844555+ryo1217intern@users.noreply.github.com>
2023-11-25 06:17:55 +0900

    処理コメントの追加.

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -6,9 +6,17 @@ import (
 	"go-rest-api/model"
 )
 
+//データベースを作成しmodelで作成した型のスキーマを作成する関数.
 func main() {
+	//dbを作成しインスタンス化する処理
 	dbConn := db.NewDB()
+
+	//プログラムが終了した際にマイグレーションが完了したことを表示する
 	defer fmt.Println("Successfully Migrated")
+
+	//データベースをクローズする処理
 	defer db.CloseDB(dbConn)
+
+	//データベースにUserとTaskスキーマを作成する.このファイルの一番大事な処理
 	dbConn.AutoMigrate(&model.User{}, &model.Task{})
 }
--- repository/user_repository.go
+++ repository/user_repository.go
@@ -6,20 +6,25 @@ import (
 	"gorm.io/gorm"
 )
 
+//usecase層にて使用するメソッドをinterfaceとして定義する
 type IUserRepository interface {
 	GetUserByEmail(user *model.User, email string) error
 	CreateUser(user *model.User) error
 }
 
+//userRepositoryの構造体でdbという名前のgorm.DBのポインター型を定義する
 type userRepository struct {
 	db *gorm.DB
 }
 
+//コンストラクター. 引数はuserRepositoryで定義した型.　戻り値はIUserRepositoryで定義した
 func NewUserRespository(db *gorm.DB) IUserRepository {
 	return &userRepository{db}
 }
 
+//GetUserByEmailの実際の処理を記述.emailの情報からUserのロウを取得する.
 func (ur *userRepository) GetUserByEmail(user *model.User, email string) error {
+	//emailは一意なので
 	if err := ur.db.Where("email=?", email).First(user).Error; err != nil {
 		return err
 	}
